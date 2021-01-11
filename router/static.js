29485e611ae954ce3ad03f2114a879c3aabe8a7f
黄冠桦 <huangguanhua@yodinfo.com>
2021-01-11 15:52:05 +0800

    feat: 更换静态资源地址

--- router/static.js
+++ router/static.js
@@ -4,14 +4,19 @@ const moduleInfo = require('../util/restype');
 
 module.exports = function(req,res,next){
   const {url} = req;
-  const urlInfo = url === '/' ? 'index.html' : url;
+  // const urlInfo = url === '/' ? 'index.html' : url;
+  const urlLength = url.length;
+  const urlInfo = url[urlLength - 1] === '/' ? url + 'index.html' : url;
   const ext = path.extname(urlInfo);
   
-  fs.readFile(path.resolve(process.cwd(), `./public/build/${urlInfo}`), async (err, data) => {
+  fs.readFile(path.resolve(process.cwd(), `/home/deploy${urlInfo}`), async (err, data) => {
     if(!err){
       const resType = await moduleInfo.contentTypeinfo(ext);
       res.writeHead(200, {'Content-Type' : `${resType}; charset=utf-8`});
       res.end(data);
+    }else if(url === '/'){
+      res.writeHead(200, {'Content-Type' : `text/html; charset=utf-8`});
+      res.end('访问地址路径不正确，需要具体到项目');
     } else {
       next(req,res);
     }
